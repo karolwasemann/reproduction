9e962406b068f8f870a20e7552f63f546ff22921
karolwasemann <kw.kwaq@gmail.com>
2021-05-06 08:44:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reprodukcja-ukqqn4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/reprodukcja-ukqqn4)
---
+++ index.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+const PIXEL_COLOR = '#e44d4d';
+
+const fakeCanvas = document.querySelector('.fake-canvas');
