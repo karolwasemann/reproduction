8aea86935161a576271456d11144ff75bdb45ac5
karolwasemann <kw.kwaq@gmail.com>
2021-05-07 08:34:18 +0200

    Done

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import './style.css';
 
 const PIXEL_COLOR = '#e44d4d';
 
-const fakeCanvas = document.querySelector('.fake-canvas');
+const fakeCanvas = document.querySelectorAll('.fake-canvas .pixel');
+const orCanvas = document.querySelectorAll('.original-canvas .pixel');
+
+orCanvas.forEach((elemnt, index) => {
+  if (elemnt.classList.contains('pixel-dot'))
+    fakeCanvas[index].classList.add('pixel-dot');
+});
