0b26e069e20f389a8369f3b11929976cdc134cbe
karolwasemann <kw.kwaq@gmail.com>
2021-05-17 08:35:32 +0200

    Done

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import './style.css';
 const PIXEL_COLOR = '#e44d4d';
 
 const fakeCanvas = document.querySelectorAll('.fake-canvas .pixel');
-const orCanvas = document.querySelectorAll('.original-canvas .pixel');
+const originalCanvas = document.querySelectorAll('.original-canvas .pixel');
 
-orCanvas.forEach((elemnt, index) => {
-  if (elemnt.classList.contains('pixel-dot'))
+originalCanvas.forEach((el, index) => {
+  if (el.classList.contains('pixel-dot'))
     fakeCanvas[index].classList.add('pixel-dot');
 });
